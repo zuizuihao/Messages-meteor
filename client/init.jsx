b3b27586be81a514d15b3098405cb87a990fb75c
zuizuihao <1165622982@qq.com>
2015-12-14 22:15:28 +0800

    Add messages CURD method.

--- client/init.jsx
+++ client/init.jsx
@@ -1,8 +1,30 @@
-if (Meteor.isClient) {
-  // This code is executed on the client only
- 
-  Meteor.startup(function () {
-    // Use Meteor.startup to render the component after the page is ready
-    React.render(<App />, document.getElementById("render-target"));
-  });
-}
+// Define a collection to hold our messages
+Messages = new Mongo.Collection("messages");
+
+Meteor.startup(function () {
+  // Use Meteor.startup to render the component after the page is ready
+  React.render(<App />, document.getElementById("render-target"));
+});
+
+
+Meteor.methods({
+  addMessage(toId, text) {
+    Messages.insert({
+      text: text,
+      createdAt: new Date(),
+      fromId: Meteor.userId(),
+      toId: toId,
+      isSeen: false
+    });
+  },
+
+  removeMessage(messageId) {
+    const message = Messages.findOne(messageId);
+    Messages.remove(messageId);
+  },
+
+  setMessageSeen(messageId, isSeen) {
+    const message = Messages.findOne(messageId);
+    Messages.update(messageId, { $set: { isSeen: isSeen} });
+  }
+});
