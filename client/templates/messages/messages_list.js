a9e5978aefad28ddc989cd3aeb5f0be763770fa5
zuizuihao <1165622982@qq.com>
2015-12-15 16:47:15 +0800

    remove unused files; redo from the discover meteor tutorial.

---
+++ client/templates/messages/messages_list.js
@@ -0,0 +1,17 @@
+var messagesData = [
+  {
+    title: 'Introducing Telescope',
+    url: 'http://sachagreif.com/introducing-telescope/'
+  }, 
+  {
+    title: 'Meteor',
+    url: 'http://meteor.com'
+  }, 
+  {
+    title: 'The Meteor Book',
+    url: 'http://themeteorbook.com'
+  }
+];
+Template.messagesList.helpers({
+  messages: messagesData
+});
